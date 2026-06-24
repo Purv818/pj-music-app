0afd9ab1c015f7186b34e25b7df496197215448f
Purv Patel <tumhara-github-purvp818@gmail.com>
2026-06-24 19:54:56 +0530

    fix Railway deployment

--- app.py
+++ app.py
@@ -456,4 +456,10 @@ if __name__ == '__main__':
             conn.executescript(f.read())
         conn.commit()
         conn.close()
-    app.run(debug=True, port=5000)
+    import os
+
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT",5000))
+        debug=false 
+    )
