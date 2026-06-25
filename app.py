b97beda1bc6d6f9be80dd774a33da3fd4dc6813e
Purv Patel <tumhara-github-purvp818@gmail.com>
2026-06-25 10:02:06 +0530

    Fix indentation main block

--- app.py
+++ app.py
@@ -460,7 +460,7 @@ if __name__ == '__main__':
 
     import os
    
-if__name__== "__main__":
+if __name__== "__main__":
      app.run(
         host="0.0.0.0",
         port=int(os.environ.get("PORT",5000))
